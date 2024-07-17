6254d51f178a27c474a9b527561b8ed57dfa4cf4
Carlos <hen9@ymail.com>
2024-07-17 15:58:47 -0300

    initial commit

---
+++ back-desafio-gz/src/main/java/com/gz/desafio_gz/DesafioGzApplication.java
@@ -0,0 +1,13 @@
+package com.gz.desafio_gz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioGzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioGzApplication.class, args);
+	}
+
+}
---
+++ back-desafio-gz/src/test/java/com/gz/desafio_gz/DesafioGzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gz.desafio_gz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioGzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
