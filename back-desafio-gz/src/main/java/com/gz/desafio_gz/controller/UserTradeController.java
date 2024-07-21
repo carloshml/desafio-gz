8707f5dc4ede3f6d3bac120e55566027525afc77
Carlos <hen9@ymail.com>
2024-07-21 15:49:49 -0300

    refactoring

--- back-desafio-gz/src/main/java/com/gz/desafio_gz/controller/UserTradeController.java
+++ back-desafio-gz/src/main/java/com/gz/desafio_gz/controller/UserTradeController.java
@@ -15,21 +15,21 @@ import com.gz.desafio_gz.service.UserTradeService;
 @RequestMapping("/userTrade")
 public class UserTradeController {
 
-    UserTradeService UserTradeService;
+    UserTradeService userTradeService;
 
     public UserTradeController(UserTradeService iqs) {
-        UserTradeService = iqs;
+        userTradeService = iqs;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserTrade> getById(@PathVariable Long id) {
-        var instrument = UserTradeService.getById(id);
+        var instrument = userTradeService.getById(id);
         return ResponseEntity.ok(instrument);
     }
 
     @GetMapping
     public ResponseEntity<List<UserTrade>> getAll() {
-        var userTradeResp = UserTradeService.getAll();
+        var userTradeResp = userTradeService.getAll();
         return ResponseEntity.ok(userTradeResp);
     }
 
@@ -39,33 +39,33 @@ public class UserTradeController {
             @PathVariable(value = "dia") String dia,
             @PathVariable(value = "page") int page,
             @PathVariable(value = "qtd") int qtd) {
-        var userTradeResp = UserTradeService.listarPorInstrumenteeData(acao, dia, page, qtd);
+        var userTradeResp = userTradeService.listarPorInstrumenteeData(acao, dia, page, qtd);
         return ResponseEntity.ok(userTradeResp);
     }
 
     @GetMapping(value = "/listarPaginado/{page}/{qtd}")
     public ResponseEntity<List<UserTrade>> listarPaginado(@PathVariable(value = "page") int page,
             @PathVariable(value = "qtd") int qtd) {
-        var userTradeResp = UserTradeService.findAllPaginado(page, qtd);
+        var userTradeResp = userTradeService.findAllPaginado(page, qtd);
         return ResponseEntity.ok(userTradeResp);
     }
 
     @GetMapping(value = "/listarQtdTotal")
     public ResponseEntity<Long> listarQtdTotal() {
-        return ResponseEntity.ok(UserTradeService.listarQtdTotal());
+        return ResponseEntity.ok(userTradeService.listarQtdTotal());
     }
 
     @GetMapping(value = "/listarPorInstrumenteeDataTotal/{acao}/{dia}")
     public ResponseEntity<Long> listarPorInstrumenteeDataTotal(
             @PathVariable(value = "acao") String acao,
             @PathVariable(value = "dia") String dia) {
-        var userTradeResp = UserTradeService.listarPorInstrumenteeDataTotal(acao, dia);
+        var userTradeResp = userTradeService.listarPorInstrumenteeDataTotal(acao, dia);
         return ResponseEntity.ok(userTradeResp);
     }
 
     @GetMapping(value = "/listarAcoesNegociadas")
     public ResponseEntity<List<String>> listarAcoesNegociadas() {
-        var userTradeResp = UserTradeService.listarAcoesNegociadas();
+        var userTradeResp = userTradeService.listarAcoesNegociadas();
         return ResponseEntity.ok(userTradeResp);
     }
 
